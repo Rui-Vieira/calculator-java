f4d81218837c81599a97495bccd0b19b2af9db6f
rui_vi <Ruimtvieira@gmail.com>
2022-07-23 17:01:07 +0100

    Calculator v1.0

---
+++ src/academy/mindswap/Calculator.java
@@ -0,0 +1,20 @@
+package academy.mindswap;
+public class Calculator {
+
+    public int sum(int num,int num2){
+        return num + num2;
+    }
+    public int subtract (int num, int num2){
+        return num + num2;
+    }
+    public int multiply( int num,int num2){
+        return num * num2;
+    }
+
+
+
+    public double divide( int num,int num2){
+        return num / num2;
+    }
+
+}
---
+++ src/academy/mindswap/Main.java
@@ -0,0 +1,18 @@
+package academy.mindswap;
+
+public class Main {
+
+        public static void main(String[] args) {
+
+            Calculator calculator = new Calculator();
+
+            int sum = calculator.sum(1, 2);
+            int subtract = calculator.subtract(4, 3);
+            int multiply = calculator.multiply(2, 3);
+            double divide = calculator.divide(6, 3); //.divide(6,3);
+
+        }
+
+        // TODO: 23/07/2022 Do scan in's to give some interaction with the terminal
+    }
+
